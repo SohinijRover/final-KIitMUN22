01f34ac699442c4197041f529457e2e6740187aa
SAGAR SATAPATHY <wishsagarks@gmail.com>
2022-07-24 10:17:30 +0530

    u

--- forms/committees_double_delegate.php
+++ forms/committees_double_delegate.php
@@ -1,5 +1,17 @@
+<option>United Nations General Assembly- Disarmament and International Security Committee (DISEC)</option>
 <option>World Health Assembly (WHA)</option>
-<option>United Nations Childrens Fund (UNICEF)</option>
 <option>United nations Human Rights Council (UNHRC)</option>
-<option>United Nations Office on Drugs and Crime (UNODC)</option>
 <option>Intergovernmental Panel on Climate Change (IPCC)</option>
+<option>United Nations Commission on the Status of Women (UNCSW)</option>
+<option>World Bank</option>
+<option>United Nations Security Council (UNSC)</option>
+<option>Joint Crisis Committee (JCC)</option>
+<option>International Press (IP)</option>
+<option>United Nations Special Political and Decolonization (SPECPOL)</option>
+<option>United Nations Organisation of Islamic Cooperation (OIC)</option>
+<option>United Nations Peacebuilding Commission</option>
+<option>United Nations Conference on Trade and Development</option>
+<option>United Nations Commission on Crime Prevention and Criminal Justice</option>
+<option>International Atomic Energy Agency</option>
+
+
--- forms/committees_single_delegate.php
+++ forms/committees_single_delegate.php
@@ -3,9 +3,15 @@
 <option>United nations Human Rights Council (UNHRC)</option>
 <option>Intergovernmental Panel on Climate Change (IPCC)</option>
 <option>United Nations Commission on the Status of Women (UNCSW)</option>
-<option>United Nations Childrens Fund (UNICEF)</option>
-<option>United Nations Office on Drugs and Crime (UNODC)</option>
 <option>World Bank</option>
 <option>United Nations Security Council (UNSC)</option>
 <option>Joint Crisis Committee (JCC)</option>
 <option>International Press (IP)</option>
+<option>United Nations Special Political and Decolonization (SPECPOL)</option>
+<option>United Nations Organisation of Islamic Cooperation (OIC)</option>
+<option>United Nations Peacebuilding Commission</option>
+<option>United Nations Conference on Trade and Development</option>
+<option>United Nations Commission on Crime Prevention and Criminal Justice</option>
+<option>International Atomic Energy Agency</option>
+
+
